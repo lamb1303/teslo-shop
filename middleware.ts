17920e8500712324fd3dff1839345f408e5b1594
lamb1303 <lamb1303@outlook.com>
2023-03-03 17:33:17 -0600

    hotfix: :ambulance: Returning response body in Middleware

--- middleware.ts
+++ middleware.ts
@@ -5,48 +5,43 @@ import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
 
-  const session: any = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
-  const validRoles = ['admin', 'super-user', 'SEO'];
-  const url = req.nextUrl.clone();
-  
-  if (req.nextUrl.pathname.startsWith('/api/admin')) {
+    const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const validRoles = ['admin', 'super-user', 'SEO'];
+    const url = req.nextUrl.clone();
+
+    if (req.nextUrl.pathname.startsWith('/api/admin')) {
+
+        if (!validRoles.includes(session.user.role)) {
+            return NextResponse.json({ message: 'Auth required' }, { status: 401 })
+        }
+    }
+
+    if (!session) {
+
+        // Obtenemos la pagina que esta siendo solicitada
+        const requestedPage = req.nextUrl.pathname;
+
+        // Mandamos el usuario a loguearse
+        url.pathname = `/auth/login`;
+
+        // Al autenticarse seguira a donde iba
+        url.search = `p=${requestedPage}`;
+
+        return NextResponse.redirect(url);
+
+    }
 
     if (!validRoles.includes(session.user.role)) {
-      return new Response(JSON.stringify({message: 'No autorizado'}), {
-        status:401,
-        headers: {
-          'Content-Type':'application/json'
-        }
-      });
+        url.pathname = '/'
+
+        return NextResponse.redirect(url);
+
     }
-  }
-  
-  if(!session){
-    
-    // Obtenemos la pagina que esta siendo solicitada
-    const requestedPage = req.nextUrl.pathname;
-    
-    // Mandamos el usuario a loguearse
-    url.pathname = `/auth/login`;
-    
-    // Al autenticarse seguira a donde iba
-    url.search = `p=${requestedPage}`;
-    
-    return NextResponse.redirect(url); 
-    
-  }
-  
-  if(!validRoles.includes(session.user.role)){
-    url.pathname = '/'
 
-    return NextResponse.redirect(url);
-
-  }
-
-  return NextResponse.next();
+    return NextResponse.next();
 
 }
 
 export const config = {
-  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*']
+    matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*']
 }
